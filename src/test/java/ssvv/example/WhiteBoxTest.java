60e8d84a2b77a34bc97ab8e8a2007403f5791027
dragos231456 <dragos.neghina2000@gmail.com>
2022-04-04 22:08:04 +0300

    wbt done

--- src/test/java/ssvv/example/WhiteBoxTest.java
+++ src/test/java/ssvv/example/WhiteBoxTest.java
@@ -1,5 +1,6 @@
 package ssvv.example;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,9 +8,7 @@ import repository.*;
 import service.Service;
 import validation.*;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +16,16 @@ public class WhiteBoxTest {
     static TemaValidator assignmentValidator = new TemaValidator();
     static TemaXMLRepository assignmentRepository = new TemaXMLRepository(assignmentValidator, "wbt_repo.xml");
     static Service service = new Service(null, assignmentRepository, null);
+
+    public void after() throws FileNotFoundException, UnsupportedEncodingException {
+        File repo = new File("wbt_repo.xml");
+        repo.delete();
+        PrintWriter writer = new PrintWriter("wbt_repo.xml", "UTF-8");
+        writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+        writer.println("<Entitati>");
+        writer.println("</Entitati>");
+        writer.close();
+    }
 
     @Test
     public void tc_1_wbt_addAssignment_NullId() {
@@ -28,5 +37,24 @@ public class WhiteBoxTest {
     public void tc_2_wbt_addAssignment_NullDescription() {
         int actual = service.saveTema("2", null, 2, 1);
         assertEquals(1, actual);
+    }
+
+    @Test
+    public void tc_2_wbt_addAssignment_NegativeDeadline() {
+        int actual = service.saveTema("2", "Description", -2, 1);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void tc_2_wbt_addAssignment_NegativeStartLine() {
+        int actual = service.saveTema("2", "Description", 2, -1);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void tc_2_wbt_addAssignment_ValidAssignment() throws FileNotFoundException, UnsupportedEncodingException {
+        int actual = service.saveTema("2", "Description", 2, 1);
+        assertEquals(0, actual);
+        after();
     }
 }
